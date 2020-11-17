eb982a32e5fba52d47fb01f31e4751b29b6ec27e
Soorya018 <arunsoorya018@gmail.com>
2020-11-17 16:09:53 +0530

    Updates

---
+++ precache-manifest.c4b7ec9c9dc7a0c85e31657016e7dc57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd52153276237c26c5ed852b0d69df2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54ed3dc893ef874a4c21",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ed3dc893ef874a4c21",
+    "url": "/static/js/main.ff3c683d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1d8b562cf1ef53c667d7a9aacd92d034",
+    "url": "/static/media/photo.1d8b562c.jpg"
+  },
+  {
+    "revision": "47a628d470859ed49bd351e4be119116",
+    "url": "/static/media/resume.47a628d4.pdf"
+  }
+]);
